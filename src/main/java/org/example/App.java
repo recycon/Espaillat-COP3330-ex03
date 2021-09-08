32581113ac5ed705560d3c6f37cfdb91bd019568
recycon <you@example.commarioespaillat@knights.ucf.edu>
2021-09-08 15:27:58 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Mario Espaillat
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println( "What is the quote?" );
+        String message = scanner.nextLine();
+        System.out.println( "Who said it?" );
+        String author = scanner.nextLine();
+        System.out.println( author + " says, " + "\"" +message+ "\"");
+
+
+    }
+}
